1cd547e37219997a6dd86f35c6824eb780f05dbd
JacobGudbjerg <JacobGudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2008-11-13 15:30:20 +0000

    New test

---
+++ trunk/MikeSheWrapper/MikeSheWrapper.DFS.UnitTest/MatrixTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using MathNet.Numerics.LinearAlgebra;
+
+
+namespace MikeSheWrapper.DFS.UnitTest
+{
+  [TestFixture]
+  public class MatrixTest
+  {
+    [Test]
+    public void ConstuctorTest()
+    {
+      Matrix M = new Matrix(10, 30);
+
+      if (M[5, 5] == 0)
+        M[5, 5] = 55;
+    }
+
+  }
+}
