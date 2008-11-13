26e63a2f2435ea318de693027e5dda3e25449a8e
JacobGudbjerg <JacobGudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2008-11-13 15:31:37 +0000

    Interface extracted

---
+++ trunk/MikeSheWrapper/MikeSheWrapper.Tools/IMatrix3d.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MikeSheWrapper.Tools
+{
+  public interface IMatrix3d
+  {
+    int LayerCount { get; }
+    MathNet.Numerics.LinearAlgebra.Matrix this[int Layer] { get; set; }
+    MathNet.Numerics.LinearAlgebra.Vector this[int Row, int Column] { get; set; }
+    double this[int Row, int Column, int Layer] { get; set; }
+  }
+}
