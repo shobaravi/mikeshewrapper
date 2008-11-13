0f0988ca76aa6fe30c206e3cba95febb24092e71
JacobGudbjerg <JacobGudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2008-11-13 15:32:06 +0000

    New test and references

---
+++ MikeSheWrapper/MikeSheWrapper.UnitTest/GridInfoTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MikeSheWrapper;
+using NUnit.Framework;
+
+namespace MikeSheWrapper.UnitTest
+{
+  [TestFixture]
+  public class GridInfoTest
+  {
+
+    [Test]
+    public void GetIndexTest()
+    {
+      Model mshe = new Model(@"F:\DHI\Data\Novana\Novomr4\Result\omr4_jag_UZ.SHE");
+
+      int Column;
+      int Row;
+
+      Assert.IsTrue(mshe.GridInfo.GetIndex(518406,6148241,out Column, out Row));
+      Assert.AreEqual(159, Column);
+      Assert.AreEqual(153, Row);    
+
+    }
+
+  }
+}
