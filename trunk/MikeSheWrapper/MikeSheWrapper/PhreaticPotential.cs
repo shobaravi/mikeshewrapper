97a1d3ec73cf35af493801c1865289c12f8943e5
JacobGudbjerg <JacobGudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2008-11-13 15:28:14 +0000

    Calculation pf phreatic potential added

---
+++ trunk/MikeSheWrapper/MikeSheWrapper/PhreaticPotential.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MikeSheWrapper.Interfaces;
+using MikeSheWrapper.Tools;
+
+namespace MikeSheWrapper
+{
+  internal class PhreaticPotential:IXYZTDataSet
+  {
+    IXYZTDataSet _potential;
+    IXYZDataSet _bottomOfCell;
+    IXYZDataSet _thicknessOfCell;
+    private double _phreaticFactor = 0.5;
+    private double _deleteValue = 1e-35;
+
+    //Buffer on the timesteps
+    Dictionary<int, PhreaticPotentialData> _bufferedData = new Dictionary<int, PhreaticPotentialData>();
+
+
+    internal PhreaticPotential(IXYZTDataSet Potential, IXYZDataSet BottomOfCell, IXYZDataSet LayerThickness)
+    {
+      _potential = Potential;
+      _bottomOfCell = BottomOfCell;
+      _thicknessOfCell = LayerThickness;
+    }
+
+
+    #region IXYZTDataSet Members
+
+    /// <summary>
+    /// Returns the phreatic potential.
+    /// Note that it returns a reference to a matrix
+    /// </summary>
+    /// <param name="TimeStep"></param>
+    /// <returns></returns>
+    public IMatrix3d TimeData(int TimeStep)
+    {
+      PhreaticPotentialData PC;
+      if (!_bufferedData.TryGetValue(TimeStep, out PC))
+      {
+        PC = new PhreaticPotentialData(_potential.TimeData(TimeStep), _bottomOfCell.Data, _thicknessOfCell.Data, _phreaticFactor, _deleteValue);
+        _bufferedData.Add(TimeStep, PC);
+      }
+      return PC;
+    }
+
+    public IMatrix3d TimeData(DateTime TimeStep)
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion
+  }
+}
---
+++ trunk/MikeSheWrapper/MikeSheWrapper/PhreaticPotentialData.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MathNet.Numerics.LinearAlgebra;
+
+using MikeSheWrapper.Tools;
+using MikeSheWrapper.Interfaces;
+
+namespace MikeSheWrapper
+{
+  internal class PhreaticPotentialData:IMatrix3d 
+  {
+    //Buffer on the layers
+    Dictionary<int, Matrix> _bufferedData = new Dictionary<int, Matrix>();
+    IMatrix3d _potential;
+    IMatrix3d _bottomOfCell;
+    IMatrix3d _thicknessOfCell;
+
+    private double _phreaticFactor;
+    private double _deletevalue;
+
+    internal PhreaticPotentialData(IMatrix3d Potential, IMatrix3d BottomOfCell, IMatrix3d ThicknessOfCell, double PhreaticFactor, double DeleteValue)
+    {
+      _potential = Potential;
+      _bottomOfCell = BottomOfCell;
+      _thicknessOfCell = ThicknessOfCell;
+      _deletevalue = DeleteValue;
+      _phreaticFactor = PhreaticFactor;
+    }
+
+
+
+    #region IMatrix3d Members
+
+    public int LayerCount
+    {
+      get { return _bottomOfCell.LayerCount; }
+    }
+
+    public Matrix this[int Layer]
+    {
+      get
+      {
+        Matrix M;
+        if (!_bufferedData.TryGetValue(Layer, out M))
+        {
+          M = new Matrix(_bottomOfCell[0].ColumnCount, _bottomOfCell[0].RowCount);
+          _bufferedData.Add(Layer, M);
+          SetMatrix(Layer, M);
+        }
+        return M;
+      }
+      set
+      {
+        throw new NotImplementedException();
+      }
+    }
+
+    public Vector this[int Row, int Column]
+    {
+      get
+      {
+        Vector V = new Vector(LayerCount);
+
+        for (int i = 0; i < LayerCount; i++)
+          V[i] = this[Row, Column, i];
+        return V;
+      }
+      set
+      {
+        throw new NotImplementedException();
+      }
+    }
+
+    public double this[int Row, int Column, int Layer]
+    {
+      get
+      {
+        return this[Layer][Row,Column];
+      }
+      set
+      {
+        throw new NotImplementedException();
+      }
+    }
+
+    /// <summary>
+    /// Sets the potential in a point
+    /// </summary>
+    /// <param name="Row"></param>
+    /// <param name="Column"></param>
+    /// <param name="Layer"></param>
+    private void SetMatrix(int Layer, Matrix M)
+    {
+      for (int Row = 0; Row < _bottomOfCell[0].RowCount; Row++)
+      {
+        for (int Column = 0; Column < _bottomOfCell[0].ColumnCount; Column++)
+        {
+          //In the bottom layer the phreatic potential is always equal to the potential
+          if (Layer != 0)
+          {
+            double HeadAboveBottom = _potential[Row, Column, Layer] - _bottomOfCell[Row, Column, Layer];
+            //Criteria 1: The distance from the bottom of the cell to the potential shall be lower 
+            //than the distance from the bottom of the cell to the potential in the cell below 
+            if (HeadAboveBottom < _bottomOfCell[Row, Column, Layer] - _potential[Row, Column, Layer - 1])
+              // Criteria 2: The potential shall be below the middle of the cell. _phreaticFactor = 0.5
+              if (HeadAboveBottom < _thicknessOfCell[Row, Column, Layer] * _phreaticFactor)
+              {
+                M[Row, Column] = _deletevalue;
+                break;
+              }
+          }
+          //The phreatic potential is equal to the potential 
+          M[Row, Column] = _potential[Row, Column, Layer];
+        }
+      }
+    }
+    
+
+    #endregion
+  }
+}
