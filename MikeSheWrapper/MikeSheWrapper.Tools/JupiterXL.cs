6e8b536e6c50fac2eadd3765e5f3915ae77850e0
JacobGudbjerg <JacobGudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2008-12-09 15:15:39 +0000

    novana read method added

--- MikeSheWrapper/MikeSheWrapper.Tools/JupiterXL.cs
+++ MikeSheWrapper/MikeSheWrapper.Tools/JupiterXL.cs
@@ -1,7 +1,48 @@
-﻿namespace MikeSheWrapper.Tools {
+﻿using MikeSheWrapper.Tools.JupiterXLTableAdapters;
+
+namespace MikeSheWrapper.Tools {
     
     
     public partial class JupiterXL {
+
+      /// <summary>
+      /// Reads in data required for the NOVANA project.
+      /// </summary>
+      /// <param name="DataBaseFileName"></param>
+      public void ReadInNovanaWells(string DataBaseFileName)
+      {
+        string ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + DataBaseFileName + ";Persist Security Info=False";
+
+        //Read in boreholes through table adapter
+        BOREHOLETableAdapter BTA = new BOREHOLETableAdapter();
+        BTA.Connection.ConnectionString = ConnectionString;
+        BTA.FillByNovana(BOREHOLE);
+
+        //Read in Intakes through table adapter
+        INTAKETableAdapter ITA = new INTAKETableAdapter();
+        ITA.Connection.ConnectionString = ConnectionString;
+        ITA.FillByNovana(INTAKE);
+
+        //Read in Screens throug the table adapter
+        SCREENTableAdapter STA = new SCREENTableAdapter();
+        STA.Connection.ConnectionString = ConnectionString;
+        STA.FillByNovana(SCREEN);
+      }
+
+      /// <summary>
+      /// Reads in the waterlevels from the database using the FillByNovana method. 
+      /// Only necessary fields are read.
+      /// </summary>
+      /// <param name="DataBaseFileName"></param>
+      public void ReadWaterLevels(string DataBaseFileName)
+      {
+        string ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + DataBaseFileName + ";Persist Security Info=False";
+        WATLEVELTableAdapter WTA = new WATLEVELTableAdapter();
+        WTA.Connection.ConnectionString = ConnectionString;
+        WTA.FillByNovana(WATLEVEL);
+      }
+
+
     }
 }
 
