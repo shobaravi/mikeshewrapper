beeae26b9fc1c0a780f63a060e84f9f6f7e0e648
JacobGudbjerg <JacobGudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2008-12-15 23:31:43 +0000

    dfswriter added

---
+++ MikeSheWrapper/MikeSheWrapper.DFS.UnitTest/WriterTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using MikeSheWrapper.DFS;
+
+namespace MikeSheWrapper.DFS.UnitTest
+{
+  [TestFixture]
+  public class WriterTest
+  {
+    [Test]
+    public void FirstTest()
+    {
+
+      DFSWriter outdata = new DFSWriter(@"..\..\..\TestData\simpelmatrixKopi.dfs2");
+
+      float[] data = new float[21];
+
+      for (int i = 0; i < data.Length; i++)
+        data[i] = 10;
+
+      outdata.WriteItemTimeStep(0, 1, data);
+      outdata.Dispose();
+
+
+    }
+  }
+}
