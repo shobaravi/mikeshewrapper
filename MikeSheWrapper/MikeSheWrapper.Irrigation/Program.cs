96156fe5120e76737b72a367bf6600bf5eca8eb0
JacobGudbjerg <JacobGudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2008-12-17 22:19:36 +0000

    made to exe

---
+++ MikeSheWrapper/MikeSheWrapper.Irrigation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+
+namespace MikeSheWrapper.Irrigation
+{
+  class Program
+  {
+    /// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+    [STAThread]
+    public static void Main(string[] args)
+    {
+      string cong = args.Aggregate<string>( (a,b)=>a+b);
+
+      XmlSerializer x = new XmlSerializer(typeof(Configuration));
+
+      Configuration Cf = (Configuration)x.Deserialize(new System.IO.FileStream(args[0], System.IO.FileMode.Open));
+      Controller C = new Controller(Cf);
+
+      C.Run();
+
+
+    }
+  }
+}
