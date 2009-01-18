3b79bffc7cf32941dfb223ca4563c669dd04d78c
JacobGudbjerg <JacobGudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2009-01-18 11:47:37 +0000

    Cleaned up and commented

--- trunk/MikeSheWrapper/MikeSheWrapper.Tools/TimeSeriesEntry.cs
+++ trunk/MikeSheWrapper/MikeSheWrapper.Tools/TimeSeriesEntry.cs
@@ -5,75 +5,93 @@ using System.Text;
 
 namespace MikeSheWrapper.Tools
 {
+  /// <summary>
+  /// A small class that can hold an entry in a time series and calculate basic statistics. 
+  /// Note. Entries are equal if the time is equal.
+  /// </summary>
   public class TimeSeriesEntry:IComparable<TimeSeriesEntry>,IEquatable<TimeSeriesEntry>
   {
-    private DateTime _time;
-    private double _value;
+    
+    /// <summary>
+    /// Constructs a time series entry
+    /// </summary>
+    /// <param name="Time"></param>
+    /// <param name="Value"></param>
+    public TimeSeriesEntry(DateTime Time, double Value)
+    {
+      this.Time = Time;
+      this.Value = Value;
+    }
 
+
+    /// <summary>
+    /// The number of boundary cells close to the simulated value. Due to LayerStatistics
+    /// </summary>
     public int BoundaryCells;
+
+    /// <summary>
+    /// The number of dry cells close to the simulated value. Due to LayerStatistics
+    /// </summary>
     public int DryCells;
-    private double _simulatedValueCell;
-    private double _simulatedValue;
 
-    public double SimulatedValueCell
-    {
-      get { return _simulatedValueCell; }
-      set { _simulatedValueCell = value; }
-    }
+    /// <summary>
+    /// Am additional simulated value for this entry. Not used in the statistics calculation. Due to LayerStatistics
+    /// </summary>
+    public double SimulatedValueCell {get;set;}
+    
+    /// <summary>
+    /// Gets and sets the time for this entry
+    /// </summary>
+    public DateTime Time { get; set; }
 
-    public double SimulatedValue
-    {
-      get { return _simulatedValue; }
-      set { _simulatedValue = value; }
-    }
+    /// <summary>
+    /// Gets and sets the value for this entry
+    /// </summary>
+    public double Value { get; set; }
 
+    /// <summary>
+    /// Gets and sets the simulated value. This value is used for the statistics calculation
+    /// </summary>
+    public double SimulatedValue {get;set;}
+
+    /// <summary>
+    /// Gets the mean error. Value - simulated value
+    /// </summary>
     public double ME
     {
       get
       {
-        return _value - _simulatedValue;
+        return Value - SimulatedValue;
       }
     }
 
+    /// <summary>
+    /// Gets the root mean square error. ME^2
+    /// </summary>
     public double RMSE
     {
       get { return Math.Pow(ME, 2.0);  }
     }
-
-    public TimeSeriesEntry(DateTime Time, double Value)
-    {
-      _time = Time;
-      _value = Value;
-    }
-
-    public DateTime Time
-    {
-      get { return _time; }
-      set { _time = value; }
-    }
-
-    public double Value
-    {
-      get { return _value; }
-      set { _value = value; }
-    }
+   
+#region System.Object overrides
 
     public override string ToString()
     {
-      return "T= " + _time.ToShortDateString() + ", V = " + _value;
+      return "T= " + Time.ToShortDateString() + ", V = " + Value;
     }
-
 
     public override int GetHashCode()
     {
-      return _time.GetHashCode();
+      return Time.GetHashCode();
     }
+
+#endregion
 
     #region IComparable<TimeSeriesEntry> Members
 
     public int CompareTo(TimeSeriesEntry other)
     {
-      return _time.CompareTo(other._time);
+      return Time.CompareTo(other.Time);
     }
 
     #endregion
@@ -82,7 +100,7 @@ namespace MikeSheWrapper.Tools
 
     public bool Equals(TimeSeriesEntry other)
     {
-      return other.Time.Equals(_time);
+      return other.Time.Equals(Time);
     }
 
     #endregion
