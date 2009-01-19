52916b2b754c0e63272ba6622a7a8013d669126d
jacobgudbjerg <jacobgudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2009-01-19 14:30:26 +0000

    configuration of shape reader started

---
+++ MikeSheWrapper/MikeSheWrapper.InputDataPreparation/ShapeReaderConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MikeSheWrapper.InputDataPreparation
+{
+    public class ShapeReaderConfiguration
+    {
+        public string WellIDHeader { get; set; }
+        public string XHeader { get; set; }
+        public string YHeader { get; set; }
+        public string ZHeader { get; set; }
+    }
+}
