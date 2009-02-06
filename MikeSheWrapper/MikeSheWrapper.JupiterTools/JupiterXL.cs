9406aaab603365a41c9b7203a3c277444fbc58a2
jacobgudbjerg <jacobgudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2009-02-06 13:10:05 +0000

    Reading of extractions initiated

--- MikeSheWrapper/MikeSheWrapper.JupiterTools/JupiterXL.cs
+++ MikeSheWrapper/MikeSheWrapper.JupiterTools/JupiterXL.cs
@@ -1,75 +1,96 @@
 ﻿using MikeSheWrapper.JupiterTools.JupiterXLTableAdapters;
 
-namespace MikeSheWrapper.JupiterTools {
-    
-    
-    public partial class JupiterXL {
+namespace MikeSheWrapper.JupiterTools
+{
 
-      /// <summary>
-      /// Reads in data required for the NOVANA project.
-      /// </summary>
-      /// <param name="DataBaseFileName"></param>
-      public void PartialReadOfWells(string DataBaseFileName)
-      {
-        string ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + DataBaseFileName + ";Persist Security Info=False";
 
-        //Read in boreholes through table adapter
-        BOREHOLETableAdapter BTA = new BOREHOLETableAdapter();
-        BTA.Connection.ConnectionString = ConnectionString;
-        BTA.FillByNovana(BOREHOLE);
+  public partial class JupiterXL
+  {
 
-        //Read in Intakes through table adapter
-        INTAKETableAdapter ITA = new INTAKETableAdapter();
-        ITA.Connection.ConnectionString = ConnectionString;
-        ITA.FillByNovana(INTAKE);
+    /// <summary>
+    /// Reads in data required for the NOVANA project.
+    /// </summary>
+    /// <param name="DataBaseFileName"></param>
+    public void PartialReadOfWells(string DataBaseFileName)
+    {
+      string ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + DataBaseFileName + ";Persist Security Info=False";
 
-        //Read in Screens throug the table adapter
-        SCREENTableAdapter STA = new SCREENTableAdapter();
-        STA.Connection.ConnectionString = ConnectionString;
-        STA.FillByNovana(SCREEN);
-      }
+      //Read in boreholes through table adapter
+      BOREHOLETableAdapter BTA = new BOREHOLETableAdapter();
+      BTA.Connection.ConnectionString = ConnectionString;
+      BTA.FillByNovana(BOREHOLE);
 
-      public void ReadInTotalWellsForNovana(string DataBaseFileName)
-      {
-        string ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + DataBaseFileName + ";Persist Security Info=False";
+      //Read in Intakes through table adapter
+      INTAKETableAdapter ITA = new INTAKETableAdapter();
+      ITA.Connection.ConnectionString = ConnectionString;
+      ITA.FillByNovana(INTAKE);
 
-        //Read in boreholes through table adapter
-        BOREHOLETableAdapter BTA = new BOREHOLETableAdapter();
-        BTA.Connection.ConnectionString = ConnectionString;
-        BTA.Fill(BOREHOLE);
+      //Read in Screens throug the table adapter
+      SCREENTableAdapter STA = new SCREENTableAdapter();
+      STA.Connection.ConnectionString = ConnectionString;
+      STA.FillByNovana(SCREEN);
+    }
 
-        //Read in Intakes through table adapter
-        INTAKETableAdapter ITA = new INTAKETableAdapter();
-        ITA.Connection.ConnectionString = ConnectionString;
-        ITA.Fill(INTAKE);
+    public void ReadInTotalWellsForNovana(string DataBaseFileName)
+    {
+      string ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + DataBaseFileName + ";Persist Security Info=False";
 
-        //Read in Screens throug the table adapter
-        SCREENTableAdapter STA = new SCREENTableAdapter();
-        STA.Connection.ConnectionString = ConnectionString;
-        STA.Fill(SCREEN);
+      //Read in boreholes through table adapter
+      BOREHOLETableAdapter BTA = new BOREHOLETableAdapter();
+      BTA.Connection.ConnectionString = ConnectionString;
+      BTA.Fill(BOREHOLE);
 
-        //Read in Casings through the table adapter
-        CASINGTableAdapter CTA = new CASINGTableAdapter();
-        CTA.Connection.ConnectionString = ConnectionString;
-        CTA.Fill(CASING);
+      //Read in Intakes through table adapter
+      INTAKETableAdapter ITA = new INTAKETableAdapter();
+      ITA.Connection.ConnectionString = ConnectionString;
+      ITA.Fill(INTAKE);
 
-      }
+      //Read in Screens throug the table adapter
+      SCREENTableAdapter STA = new SCREENTableAdapter();
+      STA.Connection.ConnectionString = ConnectionString;
+      STA.Fill(SCREEN);
 
-      /// <summary>
-      /// Reads in the waterlevels from the database using the FillByNovana method. 
-      /// Only necessary fields are read.
-      /// </summary>
-      /// <param name="DataBaseFileName"></param>
-      public void ReadWaterLevels(string DataBaseFileName)
-      {
-        string ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + DataBaseFileName + ";Persist Security Info=False";
-        WATLEVELTableAdapter WTA = new WATLEVELTableAdapter();
-        WTA.Connection.ConnectionString = ConnectionString;
-        WTA.FillByNovana(WATLEVEL);
-      }
-
+      //Read in Casings through the table adapter
+      CASINGTableAdapter CTA = new CASINGTableAdapter();
+      CTA.Connection.ConnectionString = ConnectionString;
+      CTA.Fill(CASING);
 
     }
+
+    /// <summary>
+    /// Reads in the waterlevels from the database using the FillByNovana method. 
+    /// Only necessary fields are read.
+    /// </summary>
+    /// <param name="DataBaseFileName"></param>
+    public void ReadWaterLevels(string DataBaseFileName)
+    {
+      string ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + DataBaseFileName + ";Persist Security Info=False";
+      WATLEVELTableAdapter WTA = new WATLEVELTableAdapter();
+      WTA.Connection.ConnectionString = ConnectionString;
+      WTA.FillByNovana(WATLEVEL);
+    }
+
+    /// <summary>
+    /// Read in plants and  related intakes  and extration
+    /// Tables DRWPLANT, DRWPLANTINTAKE, WRRCATHCHMENT are filled.
+    /// </summary>
+    /// <param name="DataBaseFileName"></param>
+    public void ReadExtractions(string DataBaseFileName)
+    {
+      string ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + DataBaseFileName + ";Persist Security Info=False";
+      DRWPLANTTableAdapter DTA = new DRWPLANTTableAdapter();
+      DTA.Connection.ConnectionString = ConnectionString;
+      DTA.Fill(DRWPLANT);
+
+      DRWPLANTINTAKETableAdapter DTIA = new DRWPLANTINTAKETableAdapter();
+      DTIA.Connection.ConnectionString = ConnectionString;
+      DTIA.Fill(DRWPLANTINTAKE);
+
+      WRRCATCHMENTTableAdapter WTA = new WRRCATCHMENTTableAdapter();
+      WTA.Connection.ConnectionString = ConnectionString;
+      WTA.Fill(WRRCATCHMENT);
+    }
+  }
 }
 
 
