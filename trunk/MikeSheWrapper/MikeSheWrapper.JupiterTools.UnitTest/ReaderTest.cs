5c4c3aaa6d26b88ec6370b5467f300bee503c224
jacobgudbjerg <jacobgudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2009-02-06 13:10:37 +0000

    new test project

---
+++ trunk/MikeSheWrapper/MikeSheWrapper.JupiterTools.UnitTest/ReaderTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using MikeSheWrapper.JupiterTools;
+
+namespace MikeSheWrapper.JupiterTools.UnitTest
+{
+  [TestFixture]
+  public class ReaderTest
+  {
+
+    [Test]
+    public void ReadExtractionsTest()
+    {
+      Reader.Extraction(@"C:\Kode\MikeSheWrapper\TestData\AlbertslundPcJupiter.mdb");
+    }
+  }
+}
