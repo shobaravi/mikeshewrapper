25ef383858409b65167cc99f001f465bd4eddb40
jacobgudbjerg <jacobgudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2009-03-10 16:56:29 +0000

    Now reads lithology

---
+++ trunk/MikeSheWrapper/MikeSheWrapper.JupiterTools/Lithology.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MikeSheWrapper.JupiterTools
+{
+  public class Lithology:IComparable<Lithology>
+  {
+    public double Top { get; set; }
+    public double Bottom { get; set; }
+    public string RockType { get; set; }
+    public string RockSymbol { get; set; }
+    public string TotalDescription { get; set; }
+
+    public Lithology()
+    { 
+    }
+
+
+    #region IComparable<Lithology> Members
+
+    public int CompareTo(Lithology other)
+    {
+      return Bottom.CompareTo(other.Bottom);
+    }
+
+    #endregion
+  }
+}
