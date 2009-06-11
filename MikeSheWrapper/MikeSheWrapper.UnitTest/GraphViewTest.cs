e32c0f55c2d626e5afdc12981937159cc7567c5e
jacobgudbjerg <jacobgudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2009-06-11 13:19:50 +0000

    tests with charting

---
+++ MikeSheWrapper/MikeSheWrapper.UnitTest/GraphViewTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using MikeSheWrapper.Viewer;
+using MikeSheWrapper.Tools;
+
+namespace MikeSheWrapper.UnitTest
+{
+  [TestFixture]
+  public class GraphViewTest
+  {
+
+    [Test]
+    public void ViewTest()
+    {
+      Form1 f = new Form1();
+
+      List<TimeSeriesEntry> entries = new List<TimeSeriesEntry>();
+
+      entries.Add(new TimeSeriesEntry(DateTime.Now,1));
+      entries.Add(new TimeSeriesEntry(DateTime.Now.AddDays(1),2));
+
+      f.graphView1.AddTimeSeries("navn", entries);
+      f.graphView1.AddTimeSeries("navn3", entries);
+
+      f.ShowDialog();
+
+    }
+  }
+}
---
+++ MikeSheWrapper/MikeSheWrapper.Viewer/GraphView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+using MikeSheWrapper.Tools;
+
+
+namespace MikeSheWrapper.Viewer
+{
+  public partial class GraphView : UserControl
+  {
+    public GraphView()
+    {
+      InitializeComponent();
+    }
+
+    public void AddTimeSeries(string name, List<TimeSeriesEntry> entries)
+    {
+      Series n = new Series(name);
+
+      n.Name = name;
+      n.Points.DataBindXY(entries, "Time", entries,"Value");
+      n.ChartType = SeriesChartType.FastPoint;
+      this.chart1.Series.Add(n);
+      
+      this.dataGridView1.DataSource = entries;      
+     
+    }
+  }
+}
