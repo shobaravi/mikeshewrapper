dd558467955af34bdc222dfe9bad44dc8b9a2e77
jacobgudbjerg <jacobgudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2009-07-27 14:17:56 +0000

    Buffering moved to dfs3-level

--- MikeSheWrapper/MikeSheWrapper.DFS/DFS3.cs
+++ MikeSheWrapper/MikeSheWrapper.DFS/DFS3.cs
@@ -8,6 +8,10 @@ namespace MikeSheWrapper.DFS
 {
   public class DFS3:DFS2DBase
   {
+
+    //DataBuffer. First on Item, then on timeStep. 
+    private Dictionary<int, Dictionary<int, Matrix3d>> _bufferData = new Dictionary<int, Dictionary<int, Matrix3d>>();
+
     /// <summary>
     /// Provides read access to a .DFS3 file.
     /// </summary>
@@ -25,9 +29,21 @@ namespace MikeSheWrapper.DFS
     /// <returns></returns>
     public Matrix3d GetData(int TimeStep, int Item)
     {
-      ReadItemTimeStep(TimeStep, Item);
+      Matrix3d _data;
+      Dictionary<int, Matrix3d> _timeValues;
 
-      return new Matrix3d(_numberOfRows, _numberOfColumns, _numberOfLayers, dfsdata);
+      if (!_bufferData.TryGetValue(Item, out _timeValues))
+      {
+        _timeValues = new Dictionary<int, Matrix3d>();
+        _bufferData.Add(Item, _timeValues);
+      }
+      if (!_timeValues.TryGetValue(TimeStep, out _data))
+      {
+        ReadItemTimeStep(TimeStep, Item);
+        _data = new Matrix3d(_numberOfRows, _numberOfColumns, _numberOfLayers, dfsdata);
+        _timeValues.Add(TimeStep, _data);
+      }
+      return _data;
     }
 
     /// <summary>
