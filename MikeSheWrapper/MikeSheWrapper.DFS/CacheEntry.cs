698af8db03f3056efc1c4f8d05a6aa0ed1f331ac
jacobgudbjerg <jacobgudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2009-07-29 12:16:22 +0000

    Now buffers data on dfs2 and dfs3 level in a static buffer that can only hold 25 entries

---
+++ MikeSheWrapper/MikeSheWrapper.DFS/CacheEntry.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MikeSheWrapper.Tools;
+using MathNet.Numerics.LinearAlgebra;
+
+
+namespace MikeSheWrapper.DFS
+{
+  #region Small class holding info about data and the data
+  internal class CacheEntry : IEquatable<CacheEntry>
+  {
+    internal CacheEntry(string FileName, int Item, int TimeStep)
+    {
+      this.FileName = FileName;
+      this.Item = Item;
+      this.TimeStep = TimeStep;
+    }
+
+    internal CacheEntry(string FileName, int Item, int TimeStep, Matrix Data):this(FileName, Item, TimeStep)
+    {
+      this.Data = Data;
+    }
+
+    internal CacheEntry(string FileName, int Item, int TimeStep, Matrix3d Data)
+      : this(FileName, Item, TimeStep)
+    {
+      this.Data3d = Data;
+    }
+
+    internal string FileName;
+    internal int Item;
+    internal int TimeStep;
+    internal Matrix Data;
+    internal Matrix3d Data3d;
+
+    #region IEquatable<CacheId> Members
+
+    public bool Equals(CacheEntry other)
+    {
+      return FileName.Equals(other.FileName) & Item.Equals(other.Item) & TimeStep.Equals(other.TimeStep);
+    }
+
+    #endregion
+  }
+  #endregion
+}
--- MikeSheWrapper/MikeSheWrapper.DFS/DFS3.cs
+++ MikeSheWrapper/MikeSheWrapper.DFS/DFS3.cs
@@ -8,9 +8,13 @@ namespace MikeSheWrapper.DFS
 {
   public class DFS3:DFS2DBase
   {
+    //Static list holding all Matrices read from dfs2-files
+    private static Dictionary<string, Dictionary<int, Dictionary<int, CacheEntry>>> SuperCache = new Dictionary<string, Dictionary<int, Dictionary<int, CacheEntry>>>();
+    private static LinkedList<CacheEntry> AccessList = new LinkedList<CacheEntry>();
+    public static int MaxEntriesInBuffer = 25;
 
     //DataBuffer. First on Item, then on timeStep. 
-    private Dictionary<int, Dictionary<int, Matrix3d>> _bufferData = new Dictionary<int, Dictionary<int, Matrix3d>>();
+    private Dictionary<int, Dictionary<int, CacheEntry>> _bufferData;
 
     /// <summary>
     /// Provides read access to a .DFS3 file.
@@ -18,33 +22,60 @@ namespace MikeSheWrapper.DFS
     /// <param name="DFSFileName"></param>
     public DFS3(string DFSFileName):base(DFSFileName)
     {
+      if (!SuperCache.TryGetValue(AbsoluteFileName, out _bufferData))
+      {
+        _bufferData = new Dictionary<int, Dictionary<int, CacheEntry>>();
+        SuperCache.Add(AbsoluteFileName, _bufferData);
+      }
     }
 
     /// <summary>
     /// Returns a Matrix3D with the data for the TimeStep, Item
     /// TimeStep counts from 0, Item from 1.
+    /// 
     /// </summary>
     /// <param name="TimeStep"></param>
     /// <param name="Item"></param>
     /// <returns></returns>
     public Matrix3d GetData(int TimeStep, int Item)
     {
-      Matrix3d _data;
-      Dictionary<int, Matrix3d> _timeValues;
+
+      Dictionary<int, CacheEntry> _timeValues;
+      CacheEntry cen;
 
       if (!_bufferData.TryGetValue(Item, out _timeValues))
       {
-        _timeValues = new Dictionary<int, Matrix3d>();
+        _timeValues = new Dictionary<int, CacheEntry>();
         _bufferData.Add(Item, _timeValues);
       }
-      if (!_timeValues.TryGetValue(TimeStep, out _data))
+      if (!_timeValues.TryGetValue(TimeStep, out cen))
       {
         ReadItemTimeStep(TimeStep, Item);
-        _data = new Matrix3d(_numberOfRows, _numberOfColumns, _numberOfLayers, dfsdata);
-        _timeValues.Add(TimeStep, _data);
+        Matrix3d _data = new Matrix3d(_numberOfRows, _numberOfColumns, _numberOfLayers, dfsdata);
+        cen = new CacheEntry(AbsoluteFileName, Item, TimeStep, _data);
+        _timeValues.Add(TimeStep, cen);
+        CheckBuffer();
       }
-      return _data;
+      else
+        AccessList.Remove(cen);
+
+      AccessList.AddLast(cen);
+      return cen.Data3d;
     }
+
+    /// <summary>
+    /// Removes the oldest Matrix from the dictionary if the Accesslist contains more than MaxNumberOfEntries
+    /// </summary>
+    private void CheckBuffer()
+    {
+      if (AccessList.Count > MaxEntriesInBuffer)
+      {
+        CacheEntry ToRemove = AccessList.First.Value;
+        SuperCache[ToRemove.FileName][ToRemove.Item].Remove(ToRemove.TimeStep);
+        AccessList.RemoveFirst();
+      }
+    }
+
 
     /// <summary>
     /// Gets the number of Layers
