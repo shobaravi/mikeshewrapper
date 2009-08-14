7e4d0b9d5c6f18374cc4ee20aaa315503025221d
jacobgudbjerg <jacobgudbjerg@783841f8-a590-11dd-a714-1752b6e4c161>
2009-08-14 12:09:19 +0000

    FileNames test added

---
+++ MikeSheWrapper/MikeSheWrapper.UnitTest/FileNamesTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MikeSheWrapper;
+using NUnit.Framework;
+
+
+namespace MikeSheWrapper.UnitTest
+{
+  [TestFixture]
+  public class FileNamesTest
+  {
+    private FileNames _filenames;
+    [SetUp]
+    public void Load()
+    {
+      _filenames = new FileNames(@"..\..\..\TestData\TestModelDemo.she");
+    }
+
+
+    [Test]
+    public void WellFile()
+    {
+      Assert.AreEqual(Path.GetFullPath(@"..\..\..\TestData\DemoWells.WEL"), _filenames.WelFileName);
+    }
+
+  }
+}
